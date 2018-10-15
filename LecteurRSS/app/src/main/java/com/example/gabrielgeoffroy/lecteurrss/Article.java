df4d9cd819919905a6b327a082c6011b1288bd49
CasualSpy <arnaudbegin@hotmail.com>
2018-10-14 21:44:56 -0400

    Layout et début de logique

--- LecteurRSS/app/src/main/java/com/example/gabrielgeoffroy/lecteurrss/Article.java
+++ LecteurRSS/app/src/main/java/com/example/gabrielgeoffroy/lecteurrss/Article.java
@@ -1,9 +1,12 @@
 package com.example.gabrielgeoffroy.lecteurrss;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  *
  */
-public class Article {
+public class Article implements Parcelable {
 
     //region Variables
     public String titre;
@@ -16,6 +19,38 @@ public class Article {
         this.titre = titre;
         this.description = description;
         this.link = link;
+    }
+
+    protected Article(Parcel in) {
+        titre = in.readString();
+        description = in.readString();
+        pubDate = in.readString();
+        link = in.readString();
+        guid = in.readString();
+    }
+
+    public static final Creator<Article> CREATOR = new Creator<Article>() {
+        @Override
+        public Article createFromParcel(Parcel in) {
+            return new Article(in);
+        }
+
+        @Override
+        public Article[] newArray(int size) {
+            return new Article[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+            dest.writeString(titre);
+            dest.writeString(description);
+            dest.writeString(link);
     }
     //endregion
 
--- LecteurRSS/app/src/main/java/com/example/gabrielgeoffroy/lecteurrss/DetailsActivity.java
+++ LecteurRSS/app/src/main/java/com/example/gabrielgeoffroy/lecteurrss/DetailsActivity.java
@@ -1,13 +1,67 @@
 package com.example.gabrielgeoffroy.lecteurrss;
 
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    TextView titre;
+    TextView contenuTexte;
+    Button contenuSonore;
+    Button contenuVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        titre = findViewById(R.id.titre);
+        contenuTexte = findViewById(R.id.contenuTexte);
+        contenuSonore = findViewById(R.id.contenuSonore);
+        contenuVideo = findViewById(R.id.contenuVideo);
+
+        Intent intent = getIntent();
+        final Article a = intent.getParcelableExtra("article");
+
+        if (a != null){
+            titre.setText(a.titre);
+            contenuTexte.setText(a.description);
+            if (a.link != null && a.link != ""){
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Lecteur lecteur = new Lecteur();
+
+                        try {
+                            InputStream is = lecteur.lireUrl(a.link);
+                            String mimeType = URLConnection.guessContentTypeFromStream(is);
+                            mimeType = mimeType.substring(0, mimeType.indexOf('/'));
+                            switch(mimeType){
+                                case "audio":
+                                    MediaPlayer mediaPlayer = new MediaPlayer();
+                                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                                    mediaPlayer.setDataSource(a.link);
+                                    mediaPlayer.prepare();
+                                    mediaPlayer.start();
+                                    break;
+                                case "video":
+                                    break;
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).run();
+            }
+        }
     }
 }
